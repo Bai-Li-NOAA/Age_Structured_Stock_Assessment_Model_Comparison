92c9f404892ff8b0ec87c9a2b879111fe5b25a4f
Bai Li <bai.li@noaa.gov>
2021-02-18 14:47:42 -0500

    save ss.cor

--- R/run_ss.R
+++ R/run_ss.R
@@ -94,6 +94,7 @@ run_ss <- function(maindir=maindir, subdir="SS", om_sim_num=NULL, casedir=casedi
                                                list.files(path = getwd(), pattern = c(".rdat")),
                                                list.files(path = getwd(), pattern = c(".cov")),
                                                list.files(path = getwd(), pattern = c("Report.sso")),
+                                               list.files(path = getwd(), pattern = c("ss.cor")),
                                                list.files(path = getwd(), pattern = c(".ss"))))]))
   }
   #stopCluster(cl)
